5f9dc6ca9e3171d19d271ecfb3c202349412687f
parksy-13 <psy9905@gmail.com>
2024-03-18 17:17:42 +0900

    pull받기 전 commit

--- src/entities/comments.entity.ts
+++ src/entities/comments.entity.ts
@@ -1,4 +1,13 @@
-import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  ManyToOne,
+  JoinColumn,
+} from 'typeorm';
+import { Like } from './types/commentLike.type';
+import { Card } from './cards.entity';
+import { BoardUser } from './boardUsers.entity';
 
 @Entity({ name: 'comments' })
 export class Comment {
@@ -10,4 +19,15 @@ export class Comment {
 
   @Column({ type: 'int', unique: true, nullable: false })
   sequence: string;
+
+  @Column({ type: 'enum', enum: Like })
+  like?: Like;
+
+  @ManyToOne(() => Card, (card) => card.comments)
+  @JoinColumn({ name: 'cardId', referencedColumnName: 'cardId' })
+  card: Card;
+
+  @ManyToOne(() => BoardUser, (boardUser) => boardUser.comments)
+  @JoinColumn({ name: 'buId', referencedColumnName: 'buId' })
+  boardUser: BoardUser;
 }
