67d5b0e0f5583f8e18e33c7d8c7d4665976f9ff4
lyukidon <qgx99098@gmail.com>
2024-03-18 16:53:22 +0900

    [docs] card, tag, catalog entity 추가

--- src/entities/cards.entity.ts
+++ src/entities/cards.entity.ts
@@ -1,6 +1,65 @@
-import { Entity } from "typeorm";
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  OneToMany,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+import { Catalog } from './catalogs.entity';
+import { Tag } from './tags.entity';
+import { CardUser } from './cardUsers.entity';
+
+export enum CardStatus {
+  UPCOMING = 'upcoming',
+  INPROGRESS = 'inprogress',
+  DONE = 'done',
+}
 
 @Entity()
-export class Cards {
-  
+export class Card {
+  @PrimaryGeneratedColumn()
+  cardId: number;
+
+  @ManyToOne(() => Catalog, (catalog) => catalog.cards)
+  @JoinColumn({ name: 'catalogId', referencedColumnName: 'catalogId' })
+  catalog: Catalog;
+
+  @Column({ type: 'varchar' })
+  title: string;
+
+  @Column()
+  description: string;
+
+  @Column()
+  bgColor: string;
+
+  @Column()
+  sequence: string;
+
+  @Column()
+  status: CardStatus;
+
+  @Column()
+  startDate: Date;
+
+  @Column()
+  dueDate: Date;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+
+  @OneToMany(() => Tag, (tag) => tag.card)
+  @JoinColumn({ name: 'tagId', referencedColumnName: 'tagId' })
+  tags: Tag[];
+
+
+  @OneToMany(()=> CardUser, cardUser => cardUser.card)
+  cardUsers: CardUser[]
 }
