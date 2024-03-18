d73f05188886ff879a8458ff247dcd4f3d7ac815
lyukidon <qgx99098@gmail.com>
2024-03-18 13:02:21 +0900

    [docs] add files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,46 @@
+import { SnakeNamingStrategy } from 'typeorm-naming-strategies';
+
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
+import Joi from 'joi';
+
+const typeOrmModuleOptions = {
+  useFactory: async (
+    configService: ConfigService,
+  ): Promise<TypeOrmModuleOptions> => ({
+    namingStrategy: new SnakeNamingStrategy(),
+    type: 'mysql',
+    username: configService.get('DB_USERNAME'),
+    password: configService.get('DB_PASSWORD'),
+    host: configService.get('DB_HOST'),
+    port: configService.get('DB_PORT'),
+    database: configService.get('DB_NAME'),
+    entities: [__dirname + '/**/*.entity.{js,ts}'],
+    synchronize: configService.get('DB_SYNC'),
+    logging: configService.get('DB_LOG'),
+  }),
+  inject: [ConfigService],
+};
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        JWT_SECRET_KEY: Joi.string().required(),
+        DB_USERNAME: Joi.string().required(),
+        DB_PASSWORD: Joi.string().required(),
+        DB_HOST: Joi.string().required(),
+        DB_PORT: Joi.number().required(),
+        DB_NAME: Joi.string().required(),
+        DB_SYNC: Joi.boolean().required(),
+        DB_LOG: Joi.boolean().required()
+      }),
+    }),
+    TypeOrmModule.forRootAsync(typeOrmModuleOptions)
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
