4e8593bd8fd374b74f46b43982754dca7ebea100
jindonglee <dropnowon@gmail.com>
2024-03-19 12:47:46 +0900

    [feat] 유저 업데이트 삭제 기능 추가
    - src/utils 폴더 만들어 mail,aws 폴더 만들어 놓음, 아직 작업은 시작 안했습니다.
    
    - 유저 수정은 업데이트할 패스워드나 닉네임을 입력하지 않아도 가능합니다.

--- src/user/dto/signUp.dto.ts
+++ src/user/dto/signUp.dto.ts
@@ -1,5 +1,5 @@
 import { PickType } from '@nestjs/swagger';
-import { IsNotEmpty, IsStrongPassword } from 'class-validator';
+import { IsNotEmpty, IsOptional, IsStrongPassword } from 'class-validator';
 import { User } from 'src/entities/users.entity';
 
 export class SignUpDto extends PickType(User, [
@@ -7,11 +7,15 @@ export class SignUpDto extends PickType(User, [
   'password',
   'nickName',
 ]) {
+  /**
+   * 비밀번호 확인
+   * @example "strinG!2"
+   */
   @IsStrongPassword(
     {},
     {
       message:
-        '비밀 번호는 영문 소문자, 영문 대문자, 숫자, 특수기호를 포함해 8자 이상이어야 합니다.',
+        '비밀번호 확인은 영문 소문자, 영문 대문자, 숫자, 특수기호를 포함해 8자 이상이어야 합니다.',
     },
   )
   @IsNotEmpty({ message: '비밀번호 확인을 입력해주세요.' })
